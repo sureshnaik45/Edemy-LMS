aa4990198785d53d1e0bec811f8e2326f91a8df7
sureshnaik16 <naikkimavathsuresh@gmail.com>
2025-11-06 16:55:26 +0530

    Resolved the cors issue related to patch through vercel.json

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -5,7 +5,6 @@ import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { AppContext } from "../../context/AppContext";
 import axios from "axios";
 import { toast } from "react-toastify";
-import Logger from "../Logger";
 
 const Navbar = () => {
 	const isCourseListPage = location.pathname.includes("/course-list");
@@ -55,9 +54,6 @@ const Navbar = () => {
 				className="w-28 lg:w-32  cursor-pointer"
 			/>
 			<div className="hidden md:flex items-center gap-5 text-gray-500">
-				<div className="flex items-center gap-5">
-					<Logger/>
-				</div>
 				<div className="flex items-center gap-5">
 					{user && (
 						<>
