7676df849734e1a58419a91fc8a97984dafc8f92
sureshnaik16 <naikkimavathsuresh@gmail.com>
2025-11-06 18:12:29 +0530

    If user is enrolled and click on the course redirecting him to player/ instead of course/ again

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -1,17 +1,27 @@
 import  { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { AppContext } from '../../context/AppContext'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const CourseCard = ({course}) => {
-  const {currency, calculateRating} = useContext(AppContext)
+  const {currency, calculateRating, userData} = useContext(AppContext)
+  const navigate = useNavigate();
+  const isEnrolled = userData?.enrolledCourses?.includes(course._id);
+
+  const handleClick = () => {
+    scrollTo(0,0);
+    if (isEnrolled) {
+      navigate('/player/' + course._id);
+    } else {
+      navigate('/course/' + course._id);
+    }
+  }
   return (
-    <Link to={'/course/' + course._id} onClick={()=>scrollTo(0,0)} 
-    className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'>
+    <div onClick={handleClick} 
+    className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg cursor-pointer transition-all hover:shadow-lg'>
       <img className='w-full' src={course.courseThumbnail} alt="courseThumbnail" />
       <div className='p-3 text-left'>
         <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
-        {/* <p className='text-gray-500'>{course.educator.name}</p> */}
         
         <div className='flex items-center space-x-2'>
           <p>{calculateRating(course)}</p>
@@ -24,7 +34,7 @@ const CourseCard = ({course}) => {
         </div>
         <p className='text-base font-semibold text-gray-800'>{currency} {(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
       </div>
-    </Link>
+    </div>
   )
 }
 
